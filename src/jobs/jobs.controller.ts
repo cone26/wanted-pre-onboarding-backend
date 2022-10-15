e43dcadfed753f639b87938f615c9abe3120c321
SiwonHwang <sight41siyah@gmail.com>
2022-10-15 23:08:20 +0900

    jobs 상세 조회 controller

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('jobs')
 export class JobsController {
   @Get()
   findAll(): string {
     return 'This action returns all jobs !!';
+  }
+  @Get(':id')
+  findOne(@Param() params): string {
+    return `This action returns ${params.id}`;
   }
 }
